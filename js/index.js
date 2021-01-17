7cc48d50416592911e7f0fe4c1b4df3ceec1a0be
Casey Dietz <casey.dietz@ihsmarkit.com>
2021-01-16 20:54:31 -0700

    1/3% of project MVP complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+
+const allImg = document.querySelectorAll('img')
+
+allImg.forEach((element) => {
+    element.addEventListener('mouseover', (event) => {
+        event.target.style.transform = 'scale(1.15)';
+        event.target.style.transition = 'transform 1s';
+    element.addEventListener('mouseleave', (event) => {
+        event.target.style.transform = 'scale(1)';
+    })
+    })  
+});
+
+const body = document.querySelector('body')
+
+body.addEventListener('keydown', (event) => {
+    console.log('user is pressing keyboard')
+})
+
+const navBar = document.querySelector('.main-navigation')
+
+navBar.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('prevented default')
+})
